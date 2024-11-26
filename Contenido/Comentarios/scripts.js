5f0beadc1c85d350f8ad400b3e6544d95f40ea97
verne935 <julioverne935@gmail.com>
2024-11-25 23:37:37 -0600

    Update scripts.js

--- Contenido/Comentarios/scripts.js
+++ Contenido/Comentarios/scripts.js
@@ -33,3 +33,25 @@ function enviarComentario() {
         console.error('Error:', error);
     });
 }
+
+
+
+
+// Detectar pérdida de conexión a Internet y mostrar error 404
+window.addEventListener('offline', function() {
+    // Redirigir a la página 404 cuando se pierde la conexión
+    window.location.href = './404.html';
+});
+// Detectar reconexión a Internet y redirigir a la interfaz de pedidos sin recargar
+window.addEventListener('online', function() {
+    // Redirigir a la interfaz de pedidos al reconectarse
+    window.location.href = './index.html'; // Cambia esta ruta a la URL que deseas en caso de reconexión
+});
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./service-worker.js').then(() => {
+        console.log('Service Worker registrado con éxito.');
+    }).catch((error) => {
+        console.log('Error al registrar el Service Worker:', error);
+    });
+}
