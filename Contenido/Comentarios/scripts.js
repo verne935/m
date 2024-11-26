18b84e23a85c30f83f0eb7407908d2c934351c26
verne935 <julioverne935@gmail.com>
2024-11-25 22:08:59 -0600

    Add files via upload

---
+++ Contenido/Comentarios/scripts.js
@@ -0,0 +1,35 @@
+
+function enviarComentario() {
+
+    var nombre = document.getElementById("nombre").value;
+    var apellido = document.getElementById("apellido").value;
+    var correo = document.getElementById("correo").value;
+    var comentario = document.getElementById("comentario").value;
+
+    var formData = new FormData();
+    formData.append('nombre', nombre);
+    formData.append('apellido', apellido);
+    formData.append('correo', correo);
+    formData.append('comentario', comentario);
+
+    fetch('https://formspree.io/f/mzbnoopy', {
+        method: 'POST',
+        body: formData,
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Hubo un problema al enviar el formulario.');
+        }
+        alert('¡Gracias por tu comentario!');
+        document.getElementById("nombre").value = "";
+        document.getElementById("apellido").value = "";
+        document.getElementById("correo").value = "";
+        document.getElementById("comentario").value = "";
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
